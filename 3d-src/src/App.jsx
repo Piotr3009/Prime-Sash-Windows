f2256950e37d193a16803c168c3ede0f5fa0e263
Piotr3009 <piotr.t@skylonconstruction.com>
2026-03-17 22:08:20 +0000

    3

--- 3d-src/src/App.jsx
+++ 3d-src/src/App.jsx
@@ -446,7 +446,7 @@ function Scene({ config }) {
   return (
     <>
 
-      <PerspectiveCamera makeDefault position={[1.5, 0.5, 6.0]} fov={50} />
+      <PerspectiveCamera makeDefault position={[2, 1, 3]} fov={50} />
 
       {/* Ambient */}
       <ambientLight intensity={0.56 * b} />
