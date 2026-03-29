bc157bed26031632803fb920a30a3e8ca384f8c9
Piotr3009 <piotr.t@skylonconstruction.com>
2026-03-29 23:15:20 +0100

    20

--- 3d-src/src/components/ParametricSashWindow.jsx
+++ 3d-src/src/components/ParametricSashWindow.jsx
@@ -2029,7 +2029,7 @@ export default function ParametricSashWindow({
 
           return (<>
             {/* Left fix frame */}
-            <group position={[outerLeftX, sbYBottom, sbZ]} rotation={[0, -Math.PI / 2, 0]}>
+            <group position={[outerLeftX, sbYBottom, sbZ]} rotation={[0, Math.PI / 2, 0]}>
               <StaffBead height={sbHeight} side="left" position={[0, 0, 0]} color={DEBUG_COLOR} colorInt={DEBUG_COLOR} />
             </group>
             <group position={[lmLeft, sbYBottom, sbZ]} rotation={[0, -Math.PI / 2, 0]}>
@@ -2052,7 +2052,7 @@ export default function ParametricSashWindow({
             <group position={[rmRight, sbYBottom, sbZ]} rotation={[0, Math.PI / 2, 0]}>
               <StaffBead height={sbHeight} side="left" position={[0, 0, 0]} color={DEBUG_COLOR} colorInt={DEBUG_COLOR} />
             </group>
-            <group position={[outerRightX, sbYBottom, sbZ]} rotation={[0, Math.PI / 2, 0]}>
+            <group position={[outerRightX, sbYBottom, sbZ]} rotation={[0, -Math.PI / 2, 0]}>
               <StaffBead height={sbHeight} side="right" position={[0, 0, 0]} color={DEBUG_COLOR} colorInt={DEBUG_COLOR} />
             </group>
             <StaffBeadHorizontal width={outerRightX - rmRight} position={[(rmRight + outerRightX) / 2, sbYBottom, sbZBottom]} flipZ={false} color={DEBUG_COLOR} />
